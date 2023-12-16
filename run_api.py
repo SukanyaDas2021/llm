222580b69b6632e29777c88cd734b800d0866688
Sukanya Das <sdas@e2clouds.com>
2023-12-16 11:35:37 +0530

    modified dockerfile

--- run_api.py
+++ run_api.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 from grader import perform_evaluation
+from myopenai import does_api_key_exist
 import json
 import logging
 
@@ -12,6 +13,9 @@ logger = logging.getLogger(__name__)
 
 @app.route('/api/grade', methods=['POST'])
 def grade_answer():
+    if not does_api_key_exist():
+        abort(401, 'Unauthorized: Missing OPENAI_API_KEY in .env file')
+
     data = request.json
     if data.get('question') is None or data.get('rubric_params') is None or data.get('answer') is None:
         logging.info(data)
@@ -19,7 +23,10 @@ def grade_answer():
 
     name = data['name'] if data.get('name') else 'John'
     output = perform_evaluation(name, data['question'], data['rubric_params'], data['answer'])
-    result = {"response": json.loads(output)}
+    output_json = json.loads(output)
+    if output_json.get('score'):
+        output_json['score']['total'] = sum(output_json['score'].values())
+    result = {"response": output_json}
 
     return jsonify(result), 200
 
